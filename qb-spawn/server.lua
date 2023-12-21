82d9dcecd80d85621e6f2d1ef59820541b12ca53
Caticus <154236756+caticus-mods@users.noreply.github.com>
2023-12-21 16:18:45 +0000

    Add files via upload

---
+++ qb-spawn/server.lua
@@ -0,0 +1,15 @@
+local QBCore = exports['qb-core']:GetCoreObject()
+
+QBCore.Functions.CreateCallback('qb-spawn:server:getOwnedHouses', function(_, cb, cid)
+    if cid ~= nil then
+        local houses = exports.oxmysql:executeSync('SELECT * FROM player_houses WHERE citizenid = ?', {cid})
+        if houses[1] ~= nil then
+            cb(houses)
+        else
+            cb(nil)
+        end
+    else
+        cb(nil)
+    end
+end)
+
